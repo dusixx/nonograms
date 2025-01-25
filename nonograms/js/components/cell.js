45c4875c6b7fe7bca2c0add378bfef4a28110594
dusixx <113414935+dusixx@users.noreply.github.com>
2025-01-25 04:02:47 +0200

    feat: update Cell

--- nonograms/js/components/cell.js
+++ nonograms/js/components/cell.js
@@ -1,3 +1,4 @@
+import { isPositiveInt } from '../utils/helpers.js';
 import { Element } from './base/element.js';
 
 const mouseBtn = {
@@ -22,6 +23,10 @@ const cls = {
 export class Cell extends Element {
   #highlighted;
   #discarded;
+  #valid;
+  #position; //{row, col}
+  //#onDiscard;
+  //#onHighlight;
 
   constructor(props, ...children) {
     super(props, ...children);
@@ -40,24 +45,35 @@ export class Cell extends Element {
 
   #addInteractivity = () => {
     // disable RMB context menu
-    this.addListener('contextmenu', (e) => {
-      e.preventDefault();
-    });
+    this.addListener('contextmenu', (e) => e.preventDefault());
     this.addListener('mousedown', this.#handleMouseDown);
   };
 
-  toggleHighlight() {
-    this.#highlighted = this.toggleClass(cls.highlighted);
+  toggleHighlight(force) {
+    this.#highlighted = this.toggleClass(cls.highlighted, force);
     if (this.#highlighted) {
+      //this.#onHighlight?.();
+      //
+      //TODO: for debugging only
+      if (this.valid) {
+        this.ref.style.backgroundColor = 'green';
+      }
+      //
       this.toggleClass(cls.discarded, false);
     }
   }
 
-  toggleDiscard() {
-    this.#discarded = this.toggleClass(cls.discarded);
+  toggleDiscard(force) {
+    this.#discarded = this.toggleClass(cls.discarded, force);
     if (this.#discarded) {
+      //this.#onDiscard?.();
       this.toggleClass(cls.highlighted, false);
     }
+  }
+
+  reset() {
+    this.toggleDiscard(false);
+    this.toggleHighlight(false);
   }
 
   get highlighted() {
@@ -66,5 +82,23 @@ export class Cell extends Element {
 
   get discarded() {
     return this.#discarded;
+  }
+
+  get position() {
+    return this.#position;
+  }
+
+  set position({ row, col }) {
+    if (isPositiveInt(row) && isPositiveInt(col)) {
+      this.#position = { row, col };
+    }
+  }
+
+  get valid() {
+    return this.#valid;
+  }
+
+  set valid(v) {
+    this.#valid = Boolean(v);
   }
 }
