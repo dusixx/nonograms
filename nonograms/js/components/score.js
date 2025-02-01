6dac21c2465fb2eb097f167f641756a5e299ad1f
dusixx <113414935+dusixx@users.noreply.github.com>
2025-02-02 01:36:11 +0200

    fix: Score add/update logic

---
+++ nonograms/js/components/score.js
@@ -0,0 +1,102 @@
+import { Element, SVGElement } from './base/index.js';
+import { classes as cls, localStorageKey } from '../constants/index.js';
+import {
+  isArray,
+  isNonEmptyStr,
+  isPositiveInt,
+  elapsedToTime,
+  JSONParse,
+} from '../utils/helpers.js';
+
+const column = '# puzzle complexity elapsed'.split(' ');
+
+const iconSrc = (idx) => {
+  idx = idx > 3 ? 3 : idx;
+  return `./assets/icons.svg#icon-r${idx + 1}`;
+};
+
+//
+//--------------------
+// Score
+//--------------------
+//
+
+export class Score extends Element {
+  #data = [];
+  #results;
+
+  constructor() {
+    super({ className: cls.score });
+    const header = new Element(
+      { tag: 'ul', className: cls.scoreHeader },
+      ...column.map(
+        (text) => new Element({ tag: 'li', className: cls.scoreRowItem, text })
+      )
+    );
+    this.#results = new Element({ className: cls.scoreResults });
+    this.append(header, this.#results);
+  }
+
+  add({ puzzleName, lvlName, elapsed } = '') {
+    if (
+      !isNonEmptyStr(puzzleName) ||
+      !isNonEmptyStr(lvlName) ||
+      !isPositiveInt(elapsed)
+    ) {
+      return;
+    }
+    this.#data.push({ puzzleName, lvlName, elapsed });
+    // remove the oldest
+    if (this.#data.length > 5) {
+      this.#data = this.#data.slice(1);
+    }
+  }
+
+  update() {
+    this.#results.removeChildren();
+    const sorted = [...this.#data].sort((a, b) => a.elapsed - b.elapsed);
+
+    //{ puzzleName, lvlName, elapsed }
+    const allRows = sorted.map((rowData, place) => {
+      const icon = new SVGElement('', { href: iconSrc(place) });
+
+      return new Element(
+        { tag: 'ul', className: cls.scoreRow },
+        // reward img
+        new Element({ tag: 'li', className: cls.scoreRowItem }, icon),
+        // score row items
+        ...Object.entries(rowData).map(([name, text]) => {
+          if (/^elapsed$/i.test(name)) {
+            text = elapsedToTime(text);
+          }
+          return new Element({
+            tag: 'li',
+            className: cls.scoreRowItem,
+            text,
+          });
+        })
+      );
+    });
+    if (allRows.length === 0) {
+      this.#results.append(
+        new Element({
+          tag: 'span',
+          className: cls.scoreNoResults,
+          text: '(no results)',
+        })
+      );
+    } else {
+      this.#results.append(...allRows);
+    }
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem(localStorageKey.score, JSON.stringify(this.#data));
+  }
+
+  loadFromLocalStorage() {
+    const data = JSONParse(localStorage.getItem(localStorageKey.score));
+    this.#data = isArray(data) ? data.slice(0, 5) : [];
+    this.update();
+  }
+}
