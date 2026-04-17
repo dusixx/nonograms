bffbb1d1a9ce33de28e20be3f793e1c42bc9b5f0
dusixx <113414935+dusixx@users.noreply.github.com>
2026-04-17 07:48:21 +0300

    fix: update app utils

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,5 @@
-import { Game } from '../game/game.js';
-import { header, main } from '../ui/index.js';
+import { Game } from '../components/game/game.js';
+import { header, main } from '../components/ui/index.js';
 import { showHintOnce } from './app.utils.js';
 
 document.body.append(header.ref, main.ref);
--- src/app/app.utils.js
+++ src/app/app.utils.js
@@ -1,6 +1,7 @@
-import hintImageUrl from '../../assets/hint.png';
-import { ClassName, LocalStorageKey } from '../../common/constants/index.js';
-import { modal } from '../ui/main.js';
+import hintImageUrl from '../assets/hint.png';
+import { ClassName, LocalStorageKey } from '../common/constants/index.js';
+import { modal } from '../components/ui/main.js';
+import { Element } from '../components/index.js';
 
 const MODAL_WIDTH = '250px';
 
